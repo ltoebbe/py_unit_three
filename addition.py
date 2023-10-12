904d6067f40207e1d3021775abb9351c801dd86a
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-10-12 11:58:20 -0400

    please work i beg

--- addition.py
+++ addition.py
@@ -2,17 +2,13 @@
 import math
 
 
-def area(s, a, b, c):
-    math.sqrt(2)(s * (s - a) * (s - b) * (s - c))
-
-
-def s(a, b, c):
-    (a+b+c)/2
-
-
-print(area(3, 2, 2, 2))
+def add_two(x, y):
+    total = x+y
+    print("The sum of", x, "and", y, "is", total)
 
 
 def main():
-    if __name__ == '__main__':
-        main()
+    add_two(2, 4)
+
+
+main()
--- triangle_area.py
+++ triangle_area.py
@@ -1,2 +1,15 @@
+# Liam Toebbe
+# 10/12/2023
+
+import math
 
 
+def s(a, b, c):
+    (a+b+c)/2
+
+
+def area(s, a, b, c):
+    math.sqrt(2)(s * (s - a) * (s - b) * (s - c))
+
+
+print(area(3, 2, 2, 2))

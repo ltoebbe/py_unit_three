a8546606ecd3368dbaf9e8f7b17db18e64adffa8
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-10-16 15:13:09 -0400

    ._.

--- Tests/assignment_three_tests.py
+++ Tests/assignment_three_tests.py
@@ -4,7 +4,7 @@ import assignment_three
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(52, assignment_three.calculate_total_surface_area(3, 2, 4))
 
 
 if __name__ == '__main__':
--- assignment_three.py
+++ assignment_three.py
@@ -8,21 +8,46 @@ height = input("height of prism: ")
 
 
 def calculate_lr_area(length, height):
+    """
+    calculates the area of one side on the left/right axis
+    :param length: length
+    :param height: height
+    :return: one left/right side area
+    """
     one_lr_side_area = float(length) * float(height)
     return one_lr_side_area
 
 
 def calculate_fb_area(width, height):
+    """
+    calculates the area of one side on the front/back axis
+    :param width: width
+    :param height: height
+    :return: one front/back side area
+    """
     one_fb_side_area = float(width) * float(height)
     return one_fb_side_area
 
 
 def calculate_tb_area(length, width):
+    """
+    calculates the ares of one side on the top/bottom axis
+    :param length: length
+    :param width: width
+    :return: one top/bottom side area
+    """
     one_tb_side_area = float(length) * float(width)
     return one_tb_side_area
 
 
 def calculate_total_surface_area(length, width, height):
+    """
+    calculates the total surface area of the prism
+    :param length: length
+    :param width: width
+    :param height: height
+    :return: total surface area
+    """
     total_surface_area = (calculate_lr_area(length, width) * 2) + (calculate_fb_area(width, height) * 2) + (calculate_tb_area(length, width) * 2)
     return total_surface_area
 
